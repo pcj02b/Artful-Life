433af2dbf83185a2a1c34e71d3d240b43f955fe5
pcj02b <phillip.c.jacobs@gmail.com>
2015-08-26 15:52:47 -0700

    added owner, editor and viewer to recipe model

--- artfulLifeAPI/Models/Recipe.cs
+++ artfulLifeAPI/Models/Recipe.cs
@@ -7,12 +7,13 @@ namespace artfulLifeAPI.Models
 {
     public class Recipe
     {
-        //public object _id { get; set; }
         public string name { get; set; }
         public IEnumerable<Ingredient> ingredients { get; set; }
         public IEnumerable<Prep> prep { get; set; }
         public IEnumerable<Cook> cook { get; set; }
         public Boolean included { get; set; }
         public int multiplier { get; set; }
+        public IEnumerable<string> editors { get; set; }
+        public IEnumerable<string> viewers { get; set; }
     }
 }

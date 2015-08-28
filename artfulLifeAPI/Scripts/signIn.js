2957068ab71cff013b0235c547ed203b12abd8e8
pcj02b <phillip.c.jacobs@gmail.com>
2015-08-28 13:20:40 -0700

    _id now part of recipe. multiple recipes by the same name now possible

---
+++ artfulLifeAPI/Scripts/signIn.js
@@ -0,0 +1,34 @@
+﻿function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    localStorage.setItem("user", profile.getEmail());
+    updateRecipes();
+}
+
+$(document).ready(function () {
+    $("#logOut").hide();
+    $("#logIn").click(function () {
+        $(this).hide();
+        $("#logOut").show();
+    })
+    $("#logOut").click(function () {
+        $(this).hide();
+        $("#logIn").show();
+    })
+});
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        localStorage.removeItem("user");
+        updateRecipes();
+    });
+}

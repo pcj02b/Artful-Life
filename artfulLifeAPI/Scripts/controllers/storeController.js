ee523939dfcb12cc5214dbc4f1dba2c5edad29bc
pcj02b <phillip.jacobs@outlook.com>
2017-01-18 10:48:24 -0800

    formatting fixes

--- artfulLifeAPI/Scripts/controllers/storeController.js
+++ artfulLifeAPI/Scripts/controllers/storeController.js
@@ -39,7 +39,7 @@
             $scope.stores = [];
             $scope.updateStoreIngredientList();
         }
-    }
+    };
 
     $scope.addStore = function () {
         var newStoreName = prompt("Enter the store name");
@@ -52,7 +52,7 @@
         $scope.stores.push({ name: newStoreName, defaultStore: isDefault });
         $http.put("/api/Store?user=".concat($scope.user), { _id: $scope.user, store: $scope.stores });
         $scope.updateStoreIngredientList();
-    }
+    };
     $scope.deleteStore = function (index) {
         var wasDefault = $scope.stores[index].defaultStore;
         var newStores = new Array;
@@ -60,12 +60,12 @@
         if (confirm("This will permanently remove " + $scope.stores[index].name + ".")) {
             if (wasDefault) {
                 $scope.defaultStoreIndex = -1;
-                confirm("You've deleted your default store. Please set a new default store.")
+                confirm("You've deleted your default store. Please set a new default store.");
             }
             //assign ingredients to default store
             for (var i = 0; i < $scope.storeIngredientList[index].ingredients.length; i++) {
                 for (var n = 0; n < $scope.ingredients.length; n++) {
-                    if ($scope.ingredients[n].name == $scope.storeIngredientList[index].ingredients[i].name) {
+                    if ($scope.ingredients[n].name === $scope.storeIngredientList[index].ingredients[i].name) {
                         ingredientIndex = n;
                     }
                 }
@@ -74,15 +74,15 @@
             }
 
             //decrement the store index of any ingredients in stores greater than store being deleted
-            for (var i = 0; i < $scope.ingredients.length; i++) {
+            for (i = 0; i < $scope.ingredients.length; i++) {
                 if ($scope.ingredients[i].store > index) {
                     $scope.ingredients[i].store--;
                     $http.put("/api/Ingredients", $scope.ingredients[i]);
                 }
             }
             //create new store list
-            for (var i = 0; i < $scope.stores.length; i++) {
-                if (i != index) {
+            for (i = 0; i < $scope.stores.length; i++) {
+                if (i !== index) {
                     newStores.push($scope.stores[i]);
                 }
             }
@@ -97,7 +97,7 @@
             $scope.stores = newStores;
             $scope.updateStoreIngredientList();
         }
-    }
+    };
 
     $scope.setDefaultStore = function (index) {
         for (var i = 0; i < $scope.stores.length; i++) {
@@ -105,18 +105,18 @@
         }
         $scope.stores[index].defaultStore = true;
         $scope.updateStoreIngredientList();
-    }
+    };
 
     var isInStores = function (inputIngredient) {
         var matchedIngredient = _.find($scope.ingredients, function (ingredient) {
             return ingredient.name === inputIngredient.name;
         });
-        var storeIndex = matchedIngredient && matchedIngredient.store != -1 ? matchedIngredient.store : $scope.defaultStoreIndex;
+        var storeIndex = matchedIngredient && matchedIngredient.store !== -1 ? matchedIngredient.store : $scope.defaultStoreIndex;
         var ingredientIndex = _.findIndex($scope.storeIngredientList[storeIndex].ingredients, function (ingredient) {
             return ingredient.name === inputIngredient.name && ingredient.unit === inputIngredient.unit;
         });
         return { storeIndex: storeIndex, ingredientIndex: ingredientIndex };
-    }
+    };
 
     $scope.updateStoreIngredientList = function () {
         $scope.storeIngredientList = new Array;
@@ -131,41 +131,41 @@
             if (recipe.included) {
                 angular.forEach(recipe.ingredients, function (ingredient) {
                     var isInStore = isInStores(ingredient);
-                    if (isInStore.ingredientIndex != -1) {
+                    if (isInStore.ingredientIndex !== -1) {
                         $scope.storeIngredientList[isInStore.storeIndex].ingredients[isInStore.ingredientIndex].count[0] +=
-                            (recipe.ingredient.count[0] * recipe.multiplier);
+                            recipe.ingredient.count[0] * recipe.multiplier;
                         $scope.storeIngredientList[isInStore.storeIndex].ingredients[isInStore.ingredientIndex].count[1] +=
-                            (recipe.ingredient.count[1] * recipe.multiplier);
+                            recipe.ingredient.count[1] * recipe.multiplier;
                     }
                     else {
                         $scope.storeIngredientList[isInStore.storeIndex].ingredients.push({
-                            count: [(ingredient.count[0] * recipe.multiplier),
-                                    (ingredient.count[1] * recipe.multiplier),
-                                     ingredient.count[2]],
+                            count: [ingredient.count[0] * recipe.multiplier,
+                                    ingredient.count[1] * recipe.multiplier,
+                                    ingredient.count[2]],
                             unit: ingredient.unit,
                             name: ingredient.name
                         });
-                    };
+                    }
                 });
-            };
+            }
         });
-    }
+    };
 
     $scope.incrementRecipe = function (recipeID) {
         var recipe = _.find($scope.recipes, function (recipe) {
-            return recipe._id === recipeID
+            return recipe._id === recipeID;
         });
         recipe.multiplier++;
         $scope.updateStoreIngredientList();
-    }
+    };
 
     $scope.decrementRecipe = function (recipeID) {
         var recipe = _.find($scope.recipes, function (recipe) {
-            return recipe._id === recipeID
+            return recipe._id === recipeID;
         });
         recipe.multiplier--;
         $scope.updateStoreIngredientList();
-    }
+    };
 
     $scope.saveRecipes = function () {
         angular.forEach($scope.recipes, function (recipe, index) {
@@ -173,12 +173,12 @@
                 $http.put("/api/Recipe", recipe);
             }
         });
-    }
+    };
 
     $scope.updateIngredientStore = function (ingredientIndex, storeIndex) {
         $scope.ingredients[ingredientIndex].store = storeIndex;
         $http.put("/api/Ingredients", $scope.ingredients[ingredientIndex]).success(function () {
         });
         $scope.updateStoreIngredientList();
-    }
+    };
 }]);
